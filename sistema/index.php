5712c6ce2479292d8b85a8fbd61155c49822e510
Thiago Magyori <thiagosilvamagyori@mail.com>
2023-02-27 23:19:12 -0300

    layout login criado

---
+++ sistema/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Painel - Administrativo</title>
+    <!-- CND bootstrap style-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <!--CDN bootstrap scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+
+<section class="vh-100" style="background-color: #508bfc;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card shadow-2-strong" style="border-radius: 1rem;">
+          <div class="card-body p-5 text-center">
+
+            <h3 class="mb-5">ISP Login</h3>
+
+        <form action="">
+            <div class="form-outline mb-4">
+            <label class="form-label" for="typeEmailX-2">Email</label>
+              <input type="email" id="typeEmailX-2" class="form-control" placeholder="Entre com seu email" />
+            </div>
+
+            <div class="form-outline mb-4">
+            <label class="form-label" for="typePasswordX-2">Senha</label>
+              <input type="password" id="typePasswordX-2" class="form-control" placeholder="Entre com sua senha" />
+            </div>
+
+
+            <button class="btn btn-outline-success btn-lg btn-block" type="submit">Entrar</button>
+        </form>
+           
+        <!--linha de divisão-->
+            <hr class="my-4">
+
+        <div>Recuperar senha</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
